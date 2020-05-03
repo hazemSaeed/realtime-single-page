48e05fbb0c71812f837b6f8e0c6e89469514ca86
hazemSaeed <hazemalb96@gmail.com>
2020-05-03 14:22:02 +0300

    Relationships

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Model\Question;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,6 +19,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
+    public function question() {
+        return $this->hasMany(Question::class);
+    }
 
     /**
      * The attributes that should be hidden for arrays.
